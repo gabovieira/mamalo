58874ce6347f47ea1f1a27d55b0990adf09e0be2
gabovieira <159501545+gabovieira@users.noreply.github.com>
2024-02-22 19:28:45 -0400

    proyecto carrasquel eshop

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Page</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<?php include 'header.html'; ?>
+	<?php include 'menu.html'; ?>
+
+	<?php include 'footer.html'; ?>
+    <script src="index.js"></script>
+</body>
+</html>
